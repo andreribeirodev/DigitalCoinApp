6662aa289cb5651d9df6913b91253d79017e45bc
andreribeirodev <andreeleeandro@gmail.com>
2022-06-01 20:35:11 -0300

    adiciona constants is_crypto e is_coin_default

--- app/src/main/java/com/andreribeiro/moedasdigitais/repository/CoinRepository.kt
+++ app/src/main/java/com/andreribeiro/moedasdigitais/repository/CoinRepository.kt
@@ -9,9 +9,9 @@ class CoinRepository(
     private val coinService: CoinApiClient
 ) : ICoinRepository {
 
-    override suspend fun getCoin(): List<CoinModel> {
+    override suspend fun getCoin(type: Int): List<CoinModel> {
         return withContext(Dispatchers.IO) {
-            coinService.getCoins().filter { it.type == 1 }
+            coinService.getCoins().filter { it.type == type }
         }
     }
 }
--- app/src/main/java/com/andreribeiro/moedasdigitais/repository/ICoinRepository.kt
+++ app/src/main/java/com/andreribeiro/moedasdigitais/repository/ICoinRepository.kt
@@ -3,5 +3,5 @@ package com.andreribeiro.moedasdigitais.repository
 import com.andreribeiro.moedasdigitais.model.CoinModel
 
 interface ICoinRepository {
-    suspend fun getCoin(): List<CoinModel>
+    suspend fun getCoin(type: Int): List<CoinModel>
 }
