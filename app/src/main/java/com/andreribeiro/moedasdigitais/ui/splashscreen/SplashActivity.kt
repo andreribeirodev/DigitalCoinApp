71af70eff61eb4e5a357625393b2ad66c0170fe4
andreribeirodev <andreeleeandro@gmail.com>
2022-06-01 21:36:52 -0300

    adiciona splash screen

--- app/src/main/java/com/andreribeiro/moedasdigitais/ui/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/andreribeiro/moedasdigitais/ui/splashscreen/SplashActivity.kt
@@ -1,20 +1,23 @@
-package com.andreribeiro.moedasdigitais
+package com.andreribeiro.moedasdigitais.ui.splashscreen
 
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.andreribeiro.moedasdigitais.databinding.ActivitySplashBinding
+import com.andreribeiro.moedasdigitais.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MoneyItemAdapter::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 3000)
     }
